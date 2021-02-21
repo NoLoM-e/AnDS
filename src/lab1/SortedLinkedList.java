a7bdbc185efdbcae1ec81851ad372fafe49d952c
NoLoM <dyjey18@gmail.com>
2021-02-21 16:20:52 +0300

    finished lab1

---
+++ src/lab1/SortedLinkedList.java
@@ -0,0 +1,393 @@
+package lab1;
+
+
+public class SortedLinkedList {
+    private Value head;
+    private Value tail;
+
+    private static class Value {
+        public Value next;
+        public Object value;
+
+        public Value(Integer integer) {
+            this.value = integer;
+        }
+
+        public Value(String str) {
+            this.value = str;
+        }
+    }
+
+    public void add(Integer integer) {
+        if (integer == null) {
+            System.out.println("Null is not an argument");
+            return;
+        }
+        Value elem = new Value(integer);
+
+        if (head == null) {
+            head = elem;
+            tail = elem;
+        } else {
+            if ((int) elem.value < (int) head.value) {
+                elem.next = head;
+                head = elem;
+                return;
+            }
+
+            Value v = head;
+            Value temp = v.next;
+
+            while ((int) elem.value > (int) v.value) {
+                v = v.next;
+                if (v == null) {
+                    break;
+                }
+                temp = v.next;
+            }
+            if (v == null) {
+                tail.next = elem;
+                tail = elem;
+                return;
+            }
+            v.next = elem;
+            elem.next = temp;
+        }
+    }
+
+    public void add(String str) {
+        if (str == null) {
+            System.out.println("Null is not an argument");
+            return;
+        }
+        Value elem = new Value(str);
+
+        if (head == null) {
+            head = elem;
+            tail = elem;
+        }
+        else
+        {
+
+            if (this.compareTo(elem, head) < 0)
+            {
+                elem.next = head;
+                head = elem;
+                return;
+            }
+
+            Value v = head;
+            Value temp = v.next;
+
+            while (this.compareTo(elem, v) > 0) {
+                v = v.next;
+                if (v == null) {
+                    break;
+                }
+                temp = v.next;
+            }
+            if (v == null) {
+                tail.next = elem;
+                tail = elem;
+                return;
+            }
+            v.next = elem;
+            elem.next = temp;
+        }
+    }
+
+
+    public void addTail(Integer integer) {
+        if (integer == null) {
+            System.out.println("Null is not an argument");
+            return;
+        }
+        Value elem = new Value(integer);
+
+        if (tail == null) {
+            head = elem;
+            tail = elem;
+        } else {
+            tail.next = elem;
+            tail = elem;
+        }
+    }
+
+    public void addHead(Integer integer) {
+        if (integer == null) {
+            System.out.println("Null is not an argument");
+            return;
+        }
+        Value elem = new Value(integer);
+
+        if (head == null) {
+            head = elem;
+            tail = elem;
+        } else {
+            elem.next = head;
+            head = elem;
+        }
+    }
+
+    public void remove(int pos) {
+        if (pos == 0) {
+            this.removeHead();
+            return;
+        }
+
+        Value v = head, prev = head;
+
+        for (int i = 0; i < pos; i++) {
+            if (v == null) {
+                System.out.println("Incorrect index");
+                return;
+            }
+
+            prev = v;
+            v = v.next;
+        }
+
+        if (v.next == null) {
+            this.removeTail(prev);
+            return;
+        }
+
+        prev.next = v.next;
+    }
+
+    public void remove(Integer integer) {
+        Value elem = new Value(integer);
+        Value v = head;
+        boolean deleted = false;
+
+        if (elem.value.equals(head.value)) {
+            this.removeHead();
+            return;
+        }
+
+        int i = 0;
+        while (v.next != null) {
+            if (elem.value.equals(v.next.value)) {
+                this.remove(i + 1);
+                deleted = true;
+                continue;
+            }
+            v = v.next;
+            i++;
+        }
+
+        if (!deleted) {
+            System.out.println("Object not found");
+        }
+    }
+
+    private void removeHead() {
+        head = head.next;
+    }
+
+    private void removeTail(Value v) {
+        v.next = null;
+        tail = v;
+    }
+
+
+    public Object get() {
+        return head.value;
+    }
+
+    public Object get(int pos) {
+        Value v = head;
+
+        for (int i = 0; i < pos; i++) {
+            if (v == null) {
+                System.out.println("Incorrect index");
+                return 0;
+            }
+            v = v.next;
+        }
+        return v.value;
+    }
+
+    public Object take(int pos) {
+        Value v = this.head;
+
+        for (int i = 0; i < pos; i++) {
+            if (v != null) {
+                v = v.next;
+            } else {
+                System.out.println("Incorrect position");
+                return -1;
+            }
+        }
+
+        this.remove(pos);
+
+        return v.value;
+    }
+
+    public int find(Integer integer) {
+        Value v = this.head;
+        int i = 0;
+
+        while (v != null) {
+            if (v.value.equals(integer)) {
+                return i;
+            }
+            v = v.next;
+            i++;
+        }
+
+        return -1;
+    }
+
+    public void displayAll() {
+        Value v = head;
+        while (v != null) {
+            System.out.print(v.value + " ");
+            v = v.next;
+        }
+        System.out.println();
+    }
+
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+
+    public void makeEmpty() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int listSize() {
+        Value v = this.head;
+        int i = 0;
+
+        while (v != null) {
+            v = v.next;
+            i++;
+        }
+
+        return i;
+    }
+
+    private int compareTo(Value v1, Value v2)
+    {
+        String str1 = String.valueOf(v1.value);
+        String str2 = String.valueOf(v2.value);
+
+        return str1.compareTo(str2);
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/*
+    15.	Write a method which removes duplicate items in a sorted linked list.
+    For ex., original List: (1 2 2 3 3 3 3 3 3); result List: (1 2 3).
+*/
+
+    public void removeDuplicate()
+    {
+        Value v = this.head;
+        int i = 0;
+
+        while(v.next != null)
+        {
+            if(v.next.value.equals(v.value))
+            {
+                this.remove(i+1);
+                continue;
+            }
+
+            v = v.next;
+            i++;
+        }
+    }
+
+
+    public void reverse() {
+        Value v = head, temp = v.next;
+
+        if (temp == null) {
+            Value a = head;
+            head = tail;
+            tail = a;
+            return;
+        }
+
+        while (temp.next != null) {
+            v = v.next;
+            temp = v.next;
+        }
+
+        temp.next = v;
+        v.next = null;
+        this.reverse();
+    }
+
+/*    public SortedLinkedList reverseCopy()
+    {
+        Value v = this.head;
+
+        SortedLinkedList l1 = new SortedLinkedList();
+
+        while (v != null)
+        {
+            l1.addHead(v.value);
+            v = v.next;
+        }
+        return l1;
+    }*/
+
+    public boolean equal(SortedLinkedList l1) {
+        Value v = this.head;
+        Value v1 = l1.head;
+
+        while (v != null || v1 != null) {
+            if (v.value != v1.value) {
+                return false;
+            }
+
+            v = v.next;
+            v1 = v1.next;
+        }
+
+        if (v == null && v1 == null) {
+            return true;
+        }
+
+        return false;
+    }
+}
+/*
+    public void insertDuplicate()
+    {
+        Value v = this.head;
+
+        while (v != null)
+        {
+            if(v.value != v.next.value)
+            {
+                Value elem = new Value(v.value);
+
+                elem.next = v.next;
+                v.next = elem;
+                continue;
+            }
+            else
+            {
+                while (v.value == v.next.value)
+                {
+                    v = v.next;
+                }
+            }
+
+            v = v.next;
+        }
+    }
+}
+*/
---
+++ src/lab1/lab1.java
@@ -0,0 +1,108 @@
+package lab1;
+
+
+public class lab1
+{
+    public static void main(String[] args)
+    {
+        /*SortedLinkedList ll = new SortedLinkedList();
+        ll.add(1);
+        ll.add(2);
+        ll.add(3);
+        ll.add(4);
+        ll.add(5);
+        ll.add(6);
+        ll.add(0);
+        ll.add(7);
+        ll.add(8);
+        ll.add(9);
+        ll.add(3);
+
+        System.out.println(ll.get());
+        System.out.println(ll.get(3));
+
+        ll.displayAll();
+
+        ll.remove(5);
+        ll.displayAll();
+
+        ll.addTail(10);
+        ll.displayAll();
+
+        ll.remove(10);
+        ll.displayAll();
+
+        ll.remove(Integer.valueOf(10));
+        ll.displayAll();
+
+        ll.reverse();
+        ll.displayAll();
+
+        SortedLinkedList l1 = new SortedLinkedList();
+        l1.add(9);
+        l1.add(8);
+        l1.add(7);
+        l1.add(6);
+        l1.add(4);
+        l1.add(3);
+        l1.add(3);
+        l1.add(2);
+        l1.add(1);
+        l1.add(0);
+        l1.add(3);
+
+        l1.reverse();
+        System.out.println(ll.equal(l1));
+        ll.displayAll();
+        l1.displayAll();
+
+
+        ll.displayAll();
+        System.out.println(ll.listSize());
+        System.out.println(ll.take(3));
+        ll.displayAll();
+
+        System.out.println(ll.isEmpty());
+        System.out.println(new SortedLinkedList().isEmpty());
+
+        System.out.println(ll.listSize());
+
+        System.out.println(ll.find(Integer.valueOf(10)));
+        System.out.println(ll.find(Integer.valueOf(3)));
+        System.out.println(ll.find(Integer.valueOf(12389)));
+
+        ll.displayAll();
+        ll.remove(Integer.valueOf(3));
+        ll.displayAll();
+
+        ll.add(null);*/
+
+
+
+        SortedLinkedList l2 = new SortedLinkedList();
+
+        l2.add(1);
+        l2.add(2);
+        l2.add(2);
+        l2.add(2);
+        l2.add(3);
+        l2.add(3);l2.add(3);l2.add(3);l2.add(3);l2.add(3);l2.add(3);l2.add(3);l2.add(3);
+
+        l2.displayAll();
+        l2.removeDuplicate();
+        l2.displayAll();
+
+        SortedLinkedList ls = new SortedLinkedList();
+        ls.add("Hello");
+        ls.add("world");
+        ls.add("!");
+        ls.add("world");
+        ls.add("Hello");ls.add("Hello");
+
+
+        ls.displayAll();
+        ls.removeDuplicate();
+        ls.displayAll();
+
+    }
+}
